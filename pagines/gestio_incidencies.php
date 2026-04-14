464334eff50dd7b1960baabf9883c2f9386cc3d1
chaimaebahri <chaimaebahri@iesmontsia.org>
2026-04-14 10:03:37 +0200

    Update gestio_incidencies.php

--- pagines/gestio_incidencies.php
+++ pagines/gestio_incidencies.php
@@ -1,1 +1,18 @@
+<?php
+$rolNecessari = "professor";
+require_once "../reusables/autenticació.php";
+?>
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+    <meta charset="UTF-8">
+    <title>Gestió d'incidències</title>
+</head>
+<body>
+    <h1>Gestió d'incidències</h1>
 
+    <p>Aquí anirà la llista dels dispositius que estan en incidència.</p>
+
+    <p><a href="menu.php">Tornar al menú</a></p>
+</body>
+</html>
