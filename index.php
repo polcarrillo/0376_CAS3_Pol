cabbb8dd03e583497133993475a83c6ce76ec688
chaimaebahri <chaimaebahri@iesmontsia.org>
2026-04-14 10:00:10 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,10 @@
 <?php
 session_start();
 
-/*Aquest fitxer és el punt d'entrada de l'aplicació.*/
-
-// Si NO hi ha sessió → login
-if (!isset($_SESSION["user_id"])) {
+if (!isset($_SESSION["user_id"], $_SESSION["rol"])) {
     header("Location: pagines/login.php");
     exit;
 }
 
-// Si HI ha sessió → redirigir segons rol
-if ($_SESSION["rol"] === "professor") {
-    header("Location: pagines/professor/dashboard.php");
-    exit;
-}
-
-if ($_SESSION["rol"] === "alumne") {
-    header("Location: pagines/alumne/dispositius.php");
-    exit;
-}
-
-// Si passa alguna cosa rara (rol no definit)
-session_destroy();
-header("Location: pagines/login.php");
+header("Location: pagines/menu.php");
 exit;
-?>
