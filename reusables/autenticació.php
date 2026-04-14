c0046c3eb6db1639eb910b4f47ed9c527de4d7d0
chaimaebahri <chaimaebahri@iesmontsia.org>
2026-04-14 10:00:54 +0200

    Update autenticació.php

--- reusables/autenticació.php
+++ reusables/autenticació.php
@@ -1,1 +1,13 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
+if (!isset($_SESSION["user_id"], $_SESSION["rol"])) {
+    header("Location: /pagines/login.php");
+    exit;
+}
+
+if (isset($rolNecessari) && $_SESSION["rol"] !== $rolNecessari) {
+    die("Accés no autoritzat.");
+}
