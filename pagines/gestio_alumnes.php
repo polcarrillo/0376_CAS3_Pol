1a23f936298c7b05beb5ccb6641141a7ceedc483
chaimaebahri <chaimaebahri@iesmontsia.org>
2026-04-14 10:03:21 +0200

    Update gestio_alumnes.php

--- pagines/gestio_alumnes.php
+++ pagines/gestio_alumnes.php
@@ -1,1 +1,23 @@
+<?php
+$rolNecessari = "professor";
+require_once "../reusables/autenticació.php";
+?>
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+    <meta charset="UTF-8">
+    <title>Gestió d'alumnes</title>
+</head>
+<body>
+    <h1>Gestió d'alumnes</h1>
 
+    <p>Aquí anirà la part de:</p>
+    <ul>
+        <li>Modificar dades de l'alumne</li>
+        <li>Modificar l'estat dels seus dispositius</li>
+        <li>Crear un nou alumne</li>
+    </ul>
+
+    <p><a href="menu.php">Tornar al menú</a></p>
+</body>
+</html>
