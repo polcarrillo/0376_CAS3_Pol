229297405f0db2a9f70487148842aa3335a9d150
chaimaebahri <chaimaebahri@iesmontsia.org>
2026-04-14 10:04:00 +0200

    Update header.php

--- reusables/header.php
+++ reusables/header.php
@@ -1,1 +1,12 @@
-
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+?>
+<header>
+    <h1>CAS 3 - IAW</h1>
+    <?php if (isset($_SESSION["usuari"])): ?>
+        <p>Usuari actiu: <?= htmlspecialchars($_SESSION["usuari"]) ?></p>
+    <?php endif; ?>
+    <hr>
+</header>
