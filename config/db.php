509ef9bd3d88ba0b33ec2bca6f7b539a4407a096
chaimaebahri <chaimaebahri@iesmontsia.org>
2026-04-14 10:00:30 +0200

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,6 +1,4 @@
 <?php
-declare(strict_types=1);
-
 $host = "localhost";
 $dbname = "cas3_montsia";
 $user = "cas3user";
@@ -10,12 +8,10 @@ try {
     $pdo = new PDO(
         "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
         $user,
-        $pass,
-        [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        ]
+        $pass
     );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
-    exit("Error de connexió amb la base de dades.");
+    die("Error de connexió amb la base de dades.");
 }
